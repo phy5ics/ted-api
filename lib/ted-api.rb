316353aabf359c979a608d6fc3d43d101f946ca2
John Barton <jb@phy5ics.com>
2012-09-08 18:12:50 -0400

    Added constructor methods

--- lib/ted-api.rb
+++ lib/ted-api.rb
@@ -1,7 +1,28 @@
-require "ted-api/version"
+require 'ted-api/version'
+require 'ted-api/configuration'
+require 'ted-api/client'
+require 'ted-api/error'
 
 module Ted
   module Api
-    # Your code goes here...
+    extend Configuration
+    class << self
+      # Alias for Ted::Api::Client.new
+      #
+      # @return [Ted::Api::Client]
+      def new(options={})
+        Ted::Api::Client.new(options)
+      end
+
+      # Delegate to Ted::Api::Client.new
+      def method_missing(method, *args, &block)
+        return super unless new.respond_to?(method)
+        new.send(method, *args, &block)
+      end
+
+      def respond_to?(method, include_private=false)
+        new.respond_to?(method, include_private) || super(method, include_private)
+      end
+    end
   end
 end
