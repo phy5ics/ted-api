623d4ab4c0edfff2a5e11f1ff27dbb83e264d272
John Barton <jb@phy5ics.com>
2012-09-08 18:26:28 -0400

    Added spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,42 @@
+unless ENV['CI']
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter "/spec"
+  end
+end
+require 'ted-api'
+require 'rspec'
+require 'webmock/rspec'
+
+def a_get(url)
+  a_request(:get, ted_url(url))
+end
+
+def a_post(url)
+  a_request(:post, ted_url(url))
+end
+
+def stub_get(url)
+  stub_request(:get, ted_url(url))
+end
+
+def stub_post(url)
+  stub_request(:post, ted_url(url))
+end
+
+def stub_put(url)
+  stub_request(:put, ted_url(url))
+end
+
+def fixture_path
+  File.expand_path("../fixtures", __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
+end
+
+def ted_url(url)
+  #{@client.api_key}
+  "https://api.ted.com#{url}"
+end
