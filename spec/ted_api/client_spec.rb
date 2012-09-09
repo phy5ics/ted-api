15737f08889f5820cfacb266ba26cc886a430d7c
John Barton <jb@phy5ics.com>
2012-09-09 17:32:43 -0400

    Added core and client specs

--- spec/ted_api/client_spec.rb
+++ spec/ted_api/client_spec.rb
@@ -1,25 +1,25 @@
 require 'spec_helper'
 
-describe Ted::Api::Client do
+describe TedApi::Client do
   it 'should instantiate with api key' do
     proc {
-      Ted::Api::Client.new(api_key: 'foo')
+      TedApi::Client.new(api_key: 'foo')
     }.should_not raise_exception
   end
 
   describe "api_endpoint" do
     after(:each) do
-      Ted::Api.reset
+      TedApi.reset
     end
 
     it "should default to https://api.ted.com/" do
-      client = Ted::Api::Client.new
+      client = TedApi::Client.new
       client.api_endpoint.should == 'https://api.ted.com/'
     end
 
     it "should be set " do
-      Ted::Api.api_endpoint = 'http://foo.dev'
-      client = Ted::Api::Client.new
+      TedApi.api_endpoint = 'http://foo.dev'
+      client = TedApi::Client.new
       client.api_endpoint.should == 'http://foo.dev/'
     end
   end
--- spec/ted_api_spec.rb
+++ spec/ted_api_spec.rb
@@ -1,30 +1,21 @@
 # -*- encoding: utf-8 -*-
 require 'spec_helper'
 
-describe Ted do
+describe TedApi do
   after do
-    Ted::Api.reset
+    TedApi.reset
   end
 
   describe ".respond_to?" do
     it "should be true if method exists" do
-      Ted::Api.respond_to?(:new, true).should be_true
+      TedApi.respond_to?(:new, true).should be_true
     end
   end
 
   describe ".new" do
-    it "should be a Ted::Api::Client" do
-      Ted::Api.new.should be_a Ted::Api::Client
+    it "should be a TedApi::Client" do
+      TedApi.new.should be_a TedApi::Client
     end
   end
 
-  describe ".delegate" do
-    it "should delegate missing methods to Ted::Api::Client" do
-      # stub_get("/repos/pengwynn/octokit/issues").
-      #   to_return(:status => 200, :body => fixture('v3/issues.json'))
-      # issues = Ted::Api.issues('pengwynn/octokit')
-      # issues.last.user.login.should == 'fellix'
-    end
-
-  end
 end
