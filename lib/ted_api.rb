2ec6ebe7c4a53ac3dad7d989e177f14e73a7fc0c
John Barton <jb@phy5ics.com>
2012-09-09 17:28:40 -0400

    Removed missing method check

--- lib/ted_api.rb
+++ lib/ted_api.rb
@@ -3,26 +3,18 @@ require 'ted_api/configuration'
 require 'ted_api/client'
 require 'ted_api/error'
 
-module Ted
-  module Api
-    extend Configuration
-    class << self
-      # Alias for Ted::Api::Client.new
-      #
-      # @return [Ted::Api::Client]
-      def new(options={})
-        Ted::Api::Client.new(options)
-      end
+module TedApi
+  extend Configuration
+  class << self
+    # Alias for TedApi::Client.new
+    #
+    # @return [TedApi::Client]
+    def new(options={})
+      TedApi::Client.new(options)
+    end
 
-      # Delegate to Ted::Api::Client.new
-      def method_missing(method, *args, &block)
-        return super unless new.respond_to?(method)
-        new.send(method, *args, &block)
-      end
-
-      def respond_to?(method, include_private=false)
-        new.respond_to?(method, include_private) || super(method, include_private)
-      end
+    def respond_to?(method, include_private=false)
+      new.respond_to?(method, include_private) || super(method, include_private)
     end
   end
 end
